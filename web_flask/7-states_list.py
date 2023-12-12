9ca46b192bb5b5bc71c6553b0dda30a3e94ca370
Eonvorax <mail4bishop@gmail.com>
2023-12-12 13:25:58 +0100

    Edited template to account for python changes

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,13 +8,14 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
+@app.route('/states_list', strict_slashes=False)
 def list_states():
     """
     Displays an HTML page with the list of all State objects
     """
     states = storage.all(State).values()
     sorted_states = sorted(states, key=lambda state: state.name)
+
     return render_template("7-states_list.html", states=sorted_states)
 
 
