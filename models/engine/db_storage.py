7199b68d1a54521e6b2cee2c10664e2c1807bdfb
Eonvorax <mail4bishop@gmail.com>
2023-12-12 09:41:22 +0100

    Task 7: Improve engines (with serious grafts)

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -86,3 +86,9 @@ class DBStorage:
         # Thread-local session to avoid issues when multithreading
         Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
         self.__session = scoped_session(Session)
+
+    def close(self):
+        """
+        Closes the current session
+        """
+        self.__session.remove()
--- models/state.py
+++ models/state.py
@@ -1,25 +1,32 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import os
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from os import getenv
+from models.city import City
 
 
 class State(BaseModel, Base):
     """ State class """
-
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state",
+                              cascade="all, delete")
 
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship("City", cascade="all, delete", backref="state")
     else:
+        name = ""
+
         @property
         def cities(self):
+            """Getter attribute.
+            Returns the list of City instances with state_id matching
+            the current State.id"""
             from models import storage
-            city_instances = []
-            for city in storage.all("City").values():
-                if city.state_id == self.id:
-                    city_instances.append(city)
-            return city_instances
+
+            my_cities = []
+            for city in storage.all(City).values():
+                if self.id == city.state_id:
+                    my_cities.append(city)
+            return my_cities
